055b1fa59f037bd61134945532c3b1ef67635938
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-05-12 18:01:52 +0200

    Add save/import btn

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 const output = document.getElementById('output');
 const buttons = document.querySelectorAll('.edit_button');
 
+const saveButton = document.querySelector('#save_btn')
+const importButton = document.querySelector('#import_btn')
+
 const editText = (event)=>{
     const editType= event.currentTarget.id
     document.execCommand(editType, false, null)
 }
 
+const saveText =(e)=>{
+    console.log(e.target.id)
+}
+
+const importText =(e)=>{
+    console.log(e.target.id)
+}
+
 buttons.forEach(button => button.addEventListener('click', (e) => editText(e)))
+saveButton.addEventListener('click', (e)=>saveText(e))
+importButton.addEventListener('click', (e)=>importText(e))
